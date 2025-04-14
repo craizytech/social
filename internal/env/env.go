6defb8aea7adf5e2cec918d9083868c3970e29e2
craizytech <craizytech@gmail.com>
2025-04-14 04:13:39 +0300

    The initial setup of the project

---
+++ internal/env/env.go
@@ -0,0 +1,25 @@
+package env
+
+import (
+	"os"
+)
+
+func GetString(key, fallback string) string {
+	val, ok := os.LookupEnv(key)
+
+	if !ok {
+		return fallback
+	}
+
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	val, ok := os.LookupEnv(key)
+
+	if !ok {
+		return fallback
+	}
+
+	return val
+}
